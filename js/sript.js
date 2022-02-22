0a495a84d92602cb3aac8d40fcc2e2bab09163a4
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-22 23:00:35 +0100

    end

--- js/sript.js
+++ js/sript.js
@@ -1,9 +1,9 @@
 // Genera 5 numeri random eliminando i doppioni
-
 let arrayRandom;
 
 function random (generatore, min, max) {
   let arrayRandom = [];
+
 
   do {
     const num = getRandomIntInclusive(min, max);
@@ -31,40 +31,61 @@ console.log("arrayRandom", arrayRandom)
 // Chiediamo all'utente
 const userNum = alert(`Ciao ricorda questi 5 numeri "${arrayRandom.join(' - ')}"`);
 
-const clock = setTimeout(userNumber, 3000);
+const clock = setTimeout(correction, 3000);
 
-  function correction() {
-
-    const userArray = userNumber()
-    console.log("🚀 ~ file: sript.js ~ line 64 ~ correction ~ userArray", userArray)
-
-  }
-
-  // funzioni numeri utente
+ // funzioni numeri utente
   function userNumber () {
 
-    let user;
+  let user;
 
-    // lista numeri utente
-    const userInse = []
+  // lista numeri utente
+  const userInse = []
 
     do {
 
-      //Chiediamo all'utente di inserire un numero
-      user = parseInt(prompt('Bene ora inserisci quei 5 numeri visto'));
+    //Chiediamo all'utente di inserire un numero
+    user = parseInt(prompt('Bene ora inserisci quei 5 numeri che hai visto'));
       // console.log("user", user)
 
-      // Se l'utente sbaglia viene visualizzato l'errore in caso contrario viene pushato nella lista
-      if (isNaN(user)) {
-        alert('Errore')
-      } else {
-        userInse.push(user)
-      }
+    // Se l'utente sbaglia viene visualizzato l'errore in caso contrario viene pushato nella lista
+    if (isNaN(user)) {
+      alert('Errore')
+    } else {
+      userInse.push(user)
+    }
 
     } while(userInse.length < arrayRandom.length);
 
     return userInse;
   }
 
+  // correzione
+  function correction() {
 
+    const userArray = userNumber()
+    console.log("userArray", userArray)
 
+    const verifica = []
+
+    userArray.forEach(function (element) {
+
+      // console.log("indice", index)
+
+      if(arrayRandom.includes(element)){
+        verifica.push (element)
+      }
+
+    });
+
+    console.log("verifica", verifica)
+
+    // risposte che diamo
+    if (verifica.length === 0) {
+      alert('Non hai indovinato nessun numero')
+    } else if (verifica.length === 1) {
+      alert(`Hai indovinato un solo numero ${verifica}`)
+    } else {
+      alert(`Hai indovinato questi numeri ${verifica.join(' - ')}`)
+    }
+
+  }
